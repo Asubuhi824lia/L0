c9620e304858b5005226d9e7dfdda8f295c8fb7d
Asubuhi824lia <der_anastasi84@mail.ru>
2024-01-24 15:21:54 +0700

    fix: appearance and some incorrect code sections

--- js/index.js
+++ js/index.js
@@ -35,22 +35,20 @@ let card_names = [];
 function onResize() 
 {
     if(window.innerWidth >= 1024) {
-        Array.from(document.querySelectorAll(".unavailable__card-name")).forEach(element => {
+        document.querySelectorAll(".unavailable__card-name").forEach(element => {
             element.classList.remove("card-description_font")
             element.classList.add("text_font")
         });
     
-        Array.from(document.querySelectorAll(".size")).forEach(element => {
-            element.style.visibility = "hidden";
-        })
+        document.querySelectorAll(".size").forEach(el => {el.classList.add("visibility_h");el.classList.remove("visibility_v")})
 
-        Array.from(document.querySelectorAll(".bg-color_smoky-dark")).forEach(element => {
+        document.querySelectorAll(".total__bonus, .write-off-way").forEach(element => {
             element.classList.remove("bg-color_smoky-dark")
             element.classList.add("main_bg-color-primary")
         })
 
-        let available__card = Array.from(document.querySelectorAll(".available__card-name"))
-        let unavailable__card = Array.from(document.querySelectorAll(".unavailable__card-name"))
+        let available__card   = document.querySelectorAll(".available__card-name")
+        let unavailable__card = document.querySelectorAll(".unavailable__card-name")
         card_names.forEach((card, index) => {
             if(available__card[index].innerText == card.full) return;
             else {
@@ -59,22 +57,20 @@ function onResize()
             }
         })
     } else {
-        Array.from(document.querySelectorAll(".unavailable__card-name")).forEach(element => {
+        document.querySelectorAll(".unavailable__card-name").forEach(element => {
             element.classList.remove("text_font")
             element.classList.add("card-description_font")
         });
     
-        Array.from(document.querySelectorAll(".size")).forEach(element => {
-            element.style.visibility = "visible";
-        })
+        document.querySelectorAll(".size").forEach(el => {el.classList.remove("visibility_h");el.classList.add("visibility_v")})
 
-        Array.from(document.querySelectorAll(".bg-color_smoky-dark")).forEach(element => {
-            element.classList.remove("main_bg-color-primary")
+        document.querySelectorAll(".total__bonus, .write-off-way").forEach(element => {
             element.classList.add("bg-color_smoky-dark")
+            element.classList.remove("main_bg-color-primary")
         })
 
-        let available__card = Array.from(document.querySelectorAll(".available__card-name"))
-        let unavailable__card = Array.from(document.querySelectorAll(".unavailable__card-name"))
+        let available__card = document.querySelectorAll(".available__card-name")
+        let unavailable__card = document.querySelectorAll(".unavailable__card-name")
         card_names.forEach((card, index) => {
             if(card.short) {
                 available__card[index].innerText   = card.short+"..."
@@ -86,10 +82,10 @@ function onResize()
 
 function addIncItemListener() 
 {
-    cards.forEach(item => {
-        let cardElems = document.querySelector(`.available__card.card-${item.id}`)
-        let incBtn = cardElems.getElementsByClassName("counter__inc")[0]
-        incBtn.addEventListener("click", () => incItem(cardElems, item.left))
+    cards.forEach((item,ind) => {
+        let cardElem = document.querySelectorAll(`.available__card.card`)[ind]
+        let incBtn = cardElem.getElementsByClassName("counter__inc")[0]
+        incBtn.addEventListener("click", () => incItem(cardElem, item.left))
     })
 }
 function incItem(elem, left) {
@@ -97,17 +93,17 @@ function incItem(elem, left) {
     const curNum = Number(num.innerText)
 
     if(curNum == left) return
-    if(curNum+1 == left) elem.querySelector(".counter__inc").style.color = "rgba(0, 0, 0, 0.2)"
-    if(curNum+1 == 2) elem.querySelector(".counter__dec").style.color = "rgba(0, 0, 0, 1)"
+    if(curNum+1 == left) elem.querySelector(".counter__inc").classList.add("color_gray")
+    if(curNum+1 == 2)    elem.querySelector(".counter__dec").classList.add("color_black")
     num.innerText = curNum+1
 }
 
 function addDecItemListener() 
 {
-    cards.forEach(item => {
-        let cardElems = document.querySelector(`.available__card.card-${item.id}`)
-        let decBtn = cardElems.querySelector(".counter__dec")
-        decBtn.addEventListener("click", () => decItem(cardElems, item.left))
+    cards.forEach((item,ind) => {
+        let cardElem = document.querySelectorAll(`.available__card.card`)[ind]
+        let decBtn = cardElem.querySelector(".counter__dec")
+        decBtn.addEventListener("click", () => decItem(cardElem, item.left))
     })
 }
 function decItem(elem, left) {
@@ -115,61 +111,73 @@ function decItem(elem, left) {
     const curNum = Number(num.innerText)
 
     if(curNum > 1) num.innerText = curNum-1
-    if(curNum == 1) elem.querySelector(".counter__dec").style.color = "rgba(0, 0, 0, 0.2)"
-    if(curNum == left) elem.querySelector(".counter__inc").style.color = "rgba(0, 0, 0, 1)"
+    if(curNum == left) elem.querySelector(".counter__inc").classList.add("color_black")
+    if(curNum == 1)    elem.querySelector(".counter__dec").classList.add("color_gray")
 }
 
 function addToggleFavoriteCardListener() 
 {
-    cards.forEach(item => {
-        const cardElems = document.getElementsByClassName(`card-${item.id}`)
-        Array.from(cardElems).forEach(elem => {
-            const likeBtn = elem.querySelector(".actions__to-like")
-            likeBtn.addEventListener("click", () => likeCard(Array.from(cardElems)))
-        })
+    const cardElems = document.querySelectorAll(`.card`)
+    cardElems.forEach(elem => {
+        const likeBtn = elem.querySelector(".actions__to-like")
+        likeBtn.addEventListener("click", () => likeCard(elem))
     })
 }
-function likeCard(cardElems) {
-    const likeNum = document.getElementsByClassName("tab-favorites__num")
-    const likes = Number(Array.from(likeNum)[0].innerText)
-    Array.from(likeNum).forEach(elem => elem.style.display = 'flex')
+function likeCard(card) {
+    const card_id = Array.from(card.parentElement.children).map((el,ind)=>el==card ? ind:null).filter(ind => ind!=null)[0]
+    const a_card  = document.querySelectorAll(`.available .card`)[card_id]
+    const un_card = document.querySelectorAll(`.unavailable .card`)[card_id]
     
-    cardElems.forEach(elem => {
-        const img = elem.querySelector(".actions__to-like img")
-        const isLiked = (img.attributes.src.value.includes("to_like")) ? false : true
-        if(isLiked) {
-            img.attributes.src.value = "./icons/basket/to_like.svg"
-            if(likes!=0) Array.from(likeNum).forEach(elem => elem.innerText = likes-1)
-            if(likes-1==0) Array.from(likeNum).forEach(elem => elem.style.display = 'none')
+    toggleLike(a_card)
+    toggleLike(un_card, true)
+}
+function toggleLike(elem, isChosen=false) {
+    const likeNum = document.querySelector(".tab-favorites__num")
+    const likes = Number(likeNum.textContent)
+
+    const img = elem.querySelector(".actions__to-like img")
+    const isLiked = (img.attributes.src.value.includes("to_like")) ? false : true
+    if(isLiked) {
+        img.attributes.src.value = "./icons/basket/to_like.svg"
+        if(likes!=0   && !isChosen) likeNum.textContent = likes-1;
+        if(likes-1==0 && !isChosen) {
+            likeNum.classList.remove("display_f")
+            likeNum.classList.add("display_n")
         }
-        else {
-            img.attributes.src.value = "./icons/basket/liked.svg"            
-            Array.from(likeNum).forEach(elem => elem.innerText = likes+1)
-        }
-    })
+    }
+    else {
+        img.attributes.src.value = "./icons/basket/liked.svg"
+        likeNum.classList.add("display_f")
+        likeNum.classList.remove("display_n")        
+
+        if(isChosen) likeNum.textContent = likes+1
+    }
 }
 
 function addDelCardListener() 
 {
     cards.forEach(item => {
-        const card = document.getElementsByClassName(`card-${item.id}`)
-        Array.from(card).forEach(elem => {
-            const delBtn = elem.querySelector(".actions__to-basket")
-            delBtn.addEventListener("click", () => delCard(Array.from(card), item.id))
+        const cards = document.querySelectorAll(`.card`)
+        cards.forEach(card => {
+            const delBtn = card.querySelector(".actions__to-basket")
+            delBtn.addEventListener("click", () => delCard(card, item.id))
         })
     })
 }
-function delCard(elems, index) {
+function delCard(card, index) {
     cards = cards.filter(card => card.id != index)
-    // fetch("", {method: POST, headers: {'Content-Type': 'application/json'} body: JSON.stringify(cards)})
 
-    elems.forEach(elem => elem.remove())
+    const card_id = Array.from(card.parentElement.children).map((elem,ind)=>elem==card ? ind:null).filter(ind => ind!=null)[0]
+    const a_card  = document.querySelectorAll(`.available .card`)[card_id]
+    const un_card = document.querySelectorAll(`.unavailable .card`)[card_id]
+    a_card.remove()
+    un_card.remove()
 
     const unavNum = document.getElementById("unavailable__products-list").childElementCount
     document.querySelector(".unavailable-header h4").innerText = `Отсутствуют · ${unavNum} товара`
 
-    const cartNum = document.getElementsByClassName("tab-cart__num")
-    Array.from(cartNum).forEach(elem => {
+    const cartNum = document.querySelectorAll(".tab-cart__num")
+    cartNum.forEach(elem => {
         elem.innerText = unavNum
         if(unavNum == 0) elem.remove()
     })
@@ -213,32 +221,30 @@ window.addEventListener("resize", onResize)
 
 
 document.getElementById("selectAll").addEventListener("click", (main) => {
-    Array.from(document.getElementsByClassName("available__card-checkbox")).forEach((element) => {
+    document.querySelectorAll(".available__card-checkbox").forEach((element) => {
         element.checked = main.target.checked;
     })
 })
 
 
+document.getElementById("collapseAvailable").getElementsByTagName('img')[0].style.pointerEvents = "none";
+document.getElementById("collapseUnavailable").getElementsByTagName('img')[0].style.pointerEvents = "none";
+
 document.getElementById("collapseAvailable").addEventListener("click", (el) => {
     let list = document.getElementById("available__products-list")
-    if (list.style.display != 'none') {
-        list.style.display = 'none'
-        el.target.style.transform = 'rotate(180deg)'
-    } else {
-        list.style.display = 'block'
-        el.target.style.transform = 'rotate(0deg)'
-    }
-})
 
+    list.classList.toggle("display_n")
+    list.classList.toggle("display_b")
+    el.target.getElementsByTagName('img')[0].classList.toggle("transform_rotate_half")
+    el.target.getElementsByTagName('img')[0].classList.toggle("transform_rotate_zero")
+})
 document.getElementById("collapseUnavailable").addEventListener("click", (el) => {
     let list = document.getElementById("unavailable__products-list")
-    if (list.style.display != 'none') {
-        list.style.display = 'none'
-        el.target.style.transform = 'rotate(180deg)'
-    } else {
-        list.style.display = 'block'
-        el.target.style.transform = 'rotate(0deg)'
-    }
+
+    list.classList.toggle("display_n")
+    list.classList.toggle("display_b")
+    el.target.getElementsByTagName('img')[0].classList.toggle("transform_rotate_half")
+    el.target.getElementsByTagName('img')[0].classList.toggle("transform_rotate_zero")
 })
 
 
@@ -269,38 +275,16 @@ const inputs = {
 }
 let isFieldsValid = [false, false, false, false, false]    // Все ли поля заполнены валидными значениями?
 
-document.getElementById("total__order").addEventListener("click", orderBtn => {
-    fields.forEach(field => {
-        if(!isProcessed) {
-            fields.forEach(field => {
-                const input = document.querySelector(`#${field} input`)
-                checkEmpty(input, field)
+document.getElementById("total__order").addEventListener("click", () => {
+    if(!isProcessed) {
+        fields.forEach(field => {
+            const input = document.querySelector(`#${field} input`)
+            checkFields(input, field)
 
-                if(field=="phone") 
-                    checkPhone(input)
-                else if(field=="inn") 
-                    checkInn(input)
-                else if(field=="email") 
-                    checkEmail(input)
-                else if(field=="surname" || field=="name")
-                    checkName(input, field)
-
-
-                document.querySelector(`#${field} input`).addEventListener("input", input => {
-                    checkEmpty(input.target, field)
-                    
-                    if(field=="phone") 
-                        checkPhone(input.target)
-                    else if(field=="inn") 
-                        checkInn(input.target)
-                    else if(field=="email") 
-                        checkEmail(input.target)
-                    else if(field=="surname" || field=="name")
-                        checkName(input.target, field)
-                })
-            })
-        }
-    })
+            document.querySelector(`#${field} input`)
+                .addEventListener("input", input=>checkFields(input.target, field))
+        })
+    }
     
     console.log(isFieldsValid.filter(value => value==true).length == isFieldsValid.length)
 })
@@ -319,17 +303,21 @@ fields.forEach(field => {
 })
 
 
+function checkFields(input, field) {
+    checkEmpty(input, field)
+    if     (field=="phone") checkPhone(input)
+    else if(field=="inn")   checkInn(input)
+    else if(field=="email") checkEmail(input)
+    else if(field=="surname" || field=="name") checkName(input, field)
+}
 function checkEmpty(input, field) {
-    if(input.value.length > 0) document.querySelector(`#${field} .explain`).style.visibility = 'hidden'
+    if(input.value.length > 0) document.querySelector(`#${field} .explain`).classList.add("visibility_h")
     else {
         document.querySelector(`#${field} .explain`).innerText = inputErrs[field].empty
-        document.querySelector(`#${field} .explain`).style.display = 'inline'
-        document.querySelector(`#${field} .explain`).style.visibility = 'visible'
-        document.querySelector(`#${field} .explain`).classList.add("color_red")
-        
+        document.querySelector(`#${field} .explain`).classList.add("color_red visibility_v display_i")
         if(field=="inn") {
             document.querySelector(`#inn .recipient__label-prompt`).innerText = 'ИНН для таможни'
-            document.querySelector(`#inn .recipient__label-prompt`).style.opacity = 1
+            document.querySelector(`#inn .recipient__label-prompt`).classList.add("visibility_v")
         }
     }
 }
@@ -352,8 +340,7 @@ function checkEmail(input) {
         let isMatch = input.value.match(/^[\w]{1}[\w-\.]*@[\w-]+\.[a-z]{2,4}$/i)
         if(!isMatch) {
             document.querySelector(`#email .explain`).innerText = inputErrs.email.content
-            document.querySelector(`#email .explain`).style.visibility = 'visible'
-            document.querySelector(`#email .explain`).classList.add("color_red")
+            document.querySelector(`#email .explain`).classList.add("color_red visibility_v")
             isFieldsValid[2] = false
         } else {
             inputs.email = isMatch.input
@@ -365,12 +352,12 @@ function checkPhone(input) {
     if(input.value.length > 0) {
         let isMatch = input.value.match(/^[\+]?\d\s\d{3}\s\d{3}\s\d{2}\s\d{2}$/)
         if(isMatch) {
-            document.querySelector(`#phone .explain`).style.visibility = 'hidden'
+            document.querySelector(`#phone .explain`).classList.add("visibility_h")
             isFieldsValid[3] = true
         }
         else {
             document.querySelector(`#phone .explain`).innerText = inputErrs.phone.content
-            document.querySelector(`#phone .explain`).style.visibility = 'visible'
+            document.querySelector(`#phone .explain`).classList.add("visibility_v")
             isFieldsValid[3] = false
         }
     }
@@ -388,7 +375,7 @@ function checkInn(input) {
             document.querySelector(`#inn .recipient__label-prompt`).innerText = 'ИНН'
             isFieldsValid[4] = true
         }
-        document.querySelector(`#inn .explain`).style.visibility = 'visible'
+        document.querySelector(`#inn .explain`).classList.add("visibility_v")
     }
 }
 
