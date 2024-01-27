c56b869ae10e4b4116d2c6eea65e30c0d4f0d7a2
Asubuhi824lia <der_anastasi84@mail.ru>
2024-01-27 23:02:01 +0700

    add: toggle both popups

--- js/popups.js
+++ js/popups.js
@@ -1,25 +1,45 @@
-let popupBg = document.querySelector('.popup__bg'); // Фон попап окна
-let popup   = document.querySelector('.popup');     // Само окно
-let openPopupButtons = document.querySelectorAll('.open-popup');// Кнопки для показа окна
-let closePopupButton = document.querySelector('.close-popup');  // Кнопка для скрытия окна
+let openPayPopupButtons = document.querySelectorAll('.open-popup-pay');   // Кнопки для показа окна
+let popupBgPay = document.querySelector('.popup-pay__bg'); // Фон попап окна
+let popupPay   = document.querySelector('.popup-pay');     // Само окно
+
+let openDeliveryPopupButtons = document.querySelectorAll('.open-popup-delivery');   // Кнопки для показа окна
+let popupBgDelivery = document.querySelector('.popup-delivery__bg'); // Фон попап окна
+let popupDelivery   = document.querySelector('.popup-delivery');     // Само окно        
+
+let closeDeliveryPopupButton = document.querySelector('.close-popup-delivery'); // Кнопка для скрытия окна
+let closePayPopupButton = document.querySelector('.close-popup-pay'); // Кнопка для скрытия окна
 
 
-openPopupButtons.forEach((button) => {
+openPayPopupButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         e.preventDefault();
-        popupBg.classList.add('active'); // Добавляем класс 'active' для фона
-        popup.classList.add('active'); // И для самого окна
+        popupBgPay.classList.add('active'); // Добавляем класс 'active' для фона
+        popupPay.classList.add('active'); // И для самого окна
     })
 });
-
-closePopupButton.addEventListener('click',() => {
-    popupBg.classList.remove('active'); // Убираем активный класс с фона
-    popup.classList.remove('active'); // И с окна
+openDeliveryPopupButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        popupBgDelivery.classList.add('active'); // Добавляем класс 'active' для фона
+        popupDelivery.classList.add('active'); // И для самого окна
+    })
+});
+// Убираем активные классы с фона
+closeDeliveryPopupButton.addEventListener('click',() => {
+    popupBgDelivery.classList.remove('active'); 
+    popupDelivery.classList.remove('active');
+});
+closePayPopupButton.addEventListener('click',() => {
+    popupBgPay.classList.remove('active');
+    popupPay.classList.remove('active');
 });
 // Прячем попап окно при клике на фон
 document.addEventListener('click', (e) => {
-    if(e.target === popupBg) {
-        popupBg.classList.remove('active');
-        popup.classList.remove('active');
+    if(e.target === popupBgDelivery) {
+        popupBgDelivery.classList.remove('active'); 
+        popupDelivery.classList.remove('active');
+    } else if(e.target === popupBgPay) {
+        popupBgPay.classList.remove('active');
+        popupPay.classList.remove('active');
     }
 });
